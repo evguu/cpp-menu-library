bf585a90e3fe26732993d5596216fac336f97905
Ivgesha Lag <savchenko2002je@gmail.com>
2021-03-16 23:22:47 +0300

    мелкофикс

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -26,34 +26,38 @@ int main()
 
 	Utils::setupResolution();
 	Utils::setupEncoding();
+	system(("color "s + themes[activeTheme]).c_str());
 
 	set_terminate([]() { LM_CON_SHARE_START; cout << "Произошло аварийное завершение программы. Я тоже не знаю, почему. Отладчик в помощь!" << endl; LM_CON_SHARE_END; abort(); });
 
 	LM_DECL_START(main);
 	LM_ADD_TITLE("Лабораторная 2. Использование исключений.");
 	LM_ADD_FD(USER, "Управление пользователями");
+	LM_FD_BUTTON(USER, "Выбор пользователей", []() {LM_ID(add_user)->addToStack(); });
 	LM_ADD_FD(INSU, "Управление страховыми услугами");
+	LM_FD_BUTTON(INSU, "Выбор страхового плана", []() {LM_ID(choose_insurance_service)->addToStack(); });
 	LM_ADD_FD(ADDI, "Дополнительные функции");
-
-	LM_FD_BUTTON(USER, "Добавить пользователя", []() {});
-
-	LM_FD_BUTTON(INSU, "Выбрать страховой план", []() {});
-
 	LM_FD_BUTTON(ADDI, "Сменить тему", []() {
 		system(("color "s + themes[(++activeTheme)%themes.size()]).c_str()); 
 	});
 	LM_FD_BUTTON(ADDI, "Крашнуть программу", []() {/* Кастомный терминатор не работает, потому что VS -- ересь! */ terminate(); });
 	LM_FD_BUTTON(ADDI, "Выйти из программы", []() {Menu::finish(); });
-
-	LM_ADD_TITLE("Конец меню");
-
 	LM_DECL_END;
 	LM_ID(main)->addToStack();
+
+	LM_DECL_START(add_user);
+	LM_ADD_TITLE("Выбор пользователей");
+	LM_ADD_BUTTON("Назад", []() {Menu::popStack(); });
+	LM_DECL_END;
+	
+	LM_DECL_START(choose_insurance_service);
+	LM_ADD_TITLE("Выбор страхового плана");
+	LM_ADD_BUTTON("Назад", []() {Menu::popStack(); });
+	LM_DECL_END;
 
 	//LM_ID(nonexistent_id); // Раскомментировать для исключения 3.
 	
 	Menu::run();
 	system("cls");
 	cout << "Выход из программы..." << endl;
-	cout << getMenu("");
 }
