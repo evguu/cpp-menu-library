8eed4615ef9f4f89d2632721d54df3fe3c321ebb
Ivgesha Lag <savchenko2002je@gmail.com>
2021-03-16 21:27:52 +0300

    забил на вложенность

--- MenuMacros.h
+++ MenuMacros.h
@@ -15,7 +15,9 @@
 #define LM_FD_BUTTON(id, ...) MenuElementFolder__##id##->getElements().push_back(new MenuElementFunctionButton(__VA_ARGS__));
 #define LM_FD_FIELD(id, ...) MenuElementFolder__##id##->getElements().push_back(new MenuElementEditField(__VA_ARGS__));
 #define LM_FD_CHOICE(id, ...) MenuElementFolder__##id##->getElements().push_back(new MenuElementChoice(__VA_ARGS__));
-#define LM_FD_FD(pid, id, ...) MenuElementFolder* MenuElementFolder__##id## = new MenuElementFolder(__VA_ARGS__);MenuElementFolder__##pid##->getElements().push_back(MenuElementFolder__##id##);
+
+// Добавление папок в папки настолько затруднительно, что я воздержусь.
+// #define LM_FD_FD(pid, id, ...) MenuElementFolder* MenuElementFolder__##id## = new MenuElementFolder(__VA_ARGS__);MenuElementFolder__##pid##->getElements().push_back(MenuElementFolder__##id##);
 
 // Объявления
 #define LM_DECL_START(id) { Menu *menu = getMenu(#id); if (menu) { delete menu; }; menu = newMenu(#id);
