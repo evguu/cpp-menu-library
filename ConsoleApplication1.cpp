0da8f1a4428e768e5277ad12ae661140a5966665
Ivgesha Lag <savchenko2002je@gmail.com>
2021-06-13 15:17:38 +0300

    fixed memory-related crash

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,6 +1,10 @@
 ﻿#include "LML.h"
 
-Executable* contextDialog;
+Executable* contextDialog()
+{
+	static int context = 0;
+	return DialogDecorator::apply(Executable::from([&]() {std::cout << "Ты нажал уже " << ++context << " раз. Остановись!" << std::endl; }));
+}
 
 int main()
 {
@@ -9,14 +13,11 @@ int main()
 	Display::setBuffer(new char[Console::standardMode.getColumns()*Console::standardMode.getRows()]);
 	Theme::applyCurrent();
 
-	int context = 0;
-	contextDialog =  DialogDecorator::apply(Executable::from([&]() {std::cout << "Ты нажал уже " << ++context << " раз. Остановись!" << std::endl; }));
-
 	MD_START("#main")
 		ADD(Title, "Главное меню")$
 		ADD(Button, "Субменю", Executable::from([]() { MenuManager::addToMenuStack(getMenu("#sub")); }))$
 		ADD(Button, "Простой диалог", DialogDecorator::apply(Executable::from([]() {std::cout << "Все в порядке!\n"; })))$
-		ADD(Button, "Диалог с контекстом", contextDialog)$
+		ADD(Button, "Диалог с контекстом", contextDialog())$
 		ADD(Button, "Выйти", Executable::from([]() { MenuManager::stopLoops(); }))$
 		MD_END;
 
@@ -24,7 +25,7 @@ int main()
 		ADD(Title, "Субменю")$
 		ADD(Field, "Пустое поле")$
 		ADD(Field, "Заранее заполненное поле") e->getInput() = "Данные"; $
-		ADD(Button, "Диалог с тем же контекстом", contextDialog)$
+		ADD(Button, "Диалог с тем же контекстом", contextDialog())$
 		ADD(Button, "Назад", Executable::from([]() { MenuManager::removeFromMenuStack(); }))$
 		MD_END;
 
