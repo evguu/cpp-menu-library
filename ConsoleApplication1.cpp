2eed72df819ee3f7f48b2ef18295b655565a68fb
Ivgesha Lag <savchenko2002je@gmail.com>
2021-06-24 00:58:16 +0300

    updated test

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -17,15 +17,37 @@ void mainMenu(std::shared_ptr<Menu> m)
 		.init();
 }
 
+
 void subMenu(std::shared_ptr<Menu> m)
 {
+	std::shared_ptr<Menu> m1 = std::make_shared<Menu>();
+	[](std::shared_ptr<Menu> m)
+	{
+		MenuStream(m)
+			ADD(Title, "Субменю")$
+			ADD(Field, "Пустое поле")$
+			ADD(Field, "Заранее заполненное поле") e->getInput() = "Данные"; $
+			.init();
+	}(m1);
+
+	std::shared_ptr<Menu> m2 = std::make_shared<Menu>();
+	[](std::shared_ptr<Menu> m)
+	{
+		MenuStream(m)
+			ADD(Title, "Субменю")$
+			ADD(Field, "Пустое поле")$
+			ADD(Field, "Заранее заполненное поле") e->getInput() = "Данные"; $
+			.init();
+	}(m2);
+
 	MenuStream(m)
 		ADD(Title, "Субменю")$
 		ADD(Field, "Пустое поле")$
 		ADD(Field, "Заранее заполненное поле") e->getInput() = "Данные"; $
 		ADD(Button, "Диалог с тем же контекстом", contextDialog())$
 		ADD(Button, "Назад", []() { MenuManager::removeFromMenuStack(); })$
-		(std::reinterpret_pointer_cast<Component>(getMenu("#main")))
+		(std::reinterpret_pointer_cast<Component>(m1))
+		(std::reinterpret_pointer_cast<Component>(m2))
 		.init();
 }
 
